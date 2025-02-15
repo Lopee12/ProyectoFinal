6ab0b63730c3724ef84f356d1404a9345c976622
Lopee12 <nlopezosornio@gmail.com>
2025-02-15 20:58:47 -0300

    agregamos filtrado a cargar remito

--- src/componentes/pedidos/cargar-remito/cargar-remito.component.ts
+++ src/componentes/pedidos/cargar-remito/cargar-remito.component.ts
@@ -5,6 +5,7 @@ import {
   ViewChild,
   ElementRef,
 } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { Pedido } from '../../../interfaces/Pedido.interface';
 import { PedidoService } from '../../../services/pedido.service';
 import { CommonModule } from '@angular/common';
@@ -16,7 +17,7 @@ import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-cargar-remito',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterModule],
+  imports: [CommonModule, FormsModule, RouterModule, ReactiveFormsModule],
   templateUrl: './cargar-remito.component.html',
   styleUrl: './cargar-remito.component.css',
 })
@@ -26,20 +27,47 @@ export class CargarRemitoComponent implements OnInit {
     private productoService: ProductoService,
     private ts: PedidoService
   ) {}
+
   @ViewChild('inputRecibido') inputRecibido!: ElementRef;
 
   ngOnInit(): void {
     this.listarPedidosAceptados();
+    this.cargarAnios();
   }
 
+  
   listaPedidosAceptados: Pedido[] = [];
+  listaPedidosFiltrados: Pedido[] = [];
+  anios: number[] = [];
   nuevaCantidad: number = 0;
   toastr = inject(ToastrService);
+  fb = inject(FormBuilder);
+
+  meses = [
+    { valor: 1, nombre: 'Enero' },
+    { valor: 2, nombre: 'Febrero' },
+    { valor: 3, nombre: 'Marzo' },
+    { valor: 4, nombre: 'Abril' },
+    { valor: 5, nombre: 'Mayo' },
+    { valor: 6, nombre: 'Junio' },
+    { valor: 7, nombre: 'Julio' },
+    { valor: 8, nombre: 'Agosto' },
+    { valor: 9, nombre: 'Septiembre' },
+    { valor: 10, nombre: 'Octubre' },
+    { valor: 11, nombre: 'Noviembre' },
+    { valor: 12, nombre: 'Diciembre' },
+  ];
+
+  filtroForm = this.fb.nonNullable.group({
+    mes: [''],
+    anio: [''],
+  });
 
   listarPedidosAceptados() {
     this.ts.getPedidosAceptados().subscribe({
       next: (pedidos) => {
         this.listaPedidosAceptados = pedidos;
+        this.listaPedidosFiltrados = pedidos;
       },
       error: (err) => {
         console.log('Error', err);
@@ -87,5 +115,33 @@ export class CargarRemitoComponent implements OnInit {
         });
       }
     });
+  }
+
+  cargarAnios() {
+    const anioActual = new Date().getFullYear();
+    for (let i = anioActual; i >= anioActual - 10; i--) {
+      this.anios.push(i);
+    }
+  }
+
+  aplicarFiltro() {
+    const { mes, anio } = this.filtroForm.value;
+
+    if (mes && anio) {
+      this.listaPedidosFiltrados = this.listaPedidosAceptados.filter((pedido) => {
+        // Dividir la fecha "dd/MM/yyyy"
+        const [dia, mesPedido, anioPedido] = pedido.fecha.split('/').map(Number);
+        return mesPedido === parseInt(mes, 10) && anioPedido === parseInt(anio, 10);
+      });
+    } else {
+      this.listaPedidosFiltrados = [...this.listaPedidosAceptados];
+    }
+  }
+
+  resetearFiltros() {
+
+    this.filtroForm.reset();
+    this.listaPedidosFiltrados = [...this.listaPedidosAceptados];
+
   }
 }
